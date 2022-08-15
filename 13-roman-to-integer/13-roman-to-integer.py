c2f9b70e24cc2b1e895a175bf972b9be71ac8f8f
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-08-15 14:32:49 +0300

    Time: 100 ms (16.13%), Space: 13.9 MB (76.66%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,22 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        
-        
-        nums = 0
-        i = 0
-        while i < len(s):
-            if i + 1 < len(s) and s[i] == 'I' and s[i+1] in ('V', 'X'):
-                nums += romans[s[i+1]] - romans[s[i]]
-                i += 2
-            elif i + 1 < len(s) and s[i] == 'X' and s[i+1] in ('L', 'C'):
-                nums += romans[s[i+1]] - romans[s[i]]
-                i += 2
-            elif i + 1 < len(s) and s[i] == 'C' and s[i+1] in ('D', 'M'):
-                nums += romans[s[i+1]] - romans[s[i]]
-                i += 2
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+
+        answer = roman_dict[s[-1]]
+        size = len(s) - 2
+        while size >= 0 :
+            if roman_dict[s[size]] < roman_dict[s[size+1]]:
+                answer -= roman_dict[s[size]]
             else:
-                nums += romans[s[i]]
-                i += 1
-                
-        return nums
+                answer += roman_dict[s[size]]
+            size -=1
+        return answer
