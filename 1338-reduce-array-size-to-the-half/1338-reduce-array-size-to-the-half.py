b410c048ccf347278cf9f0f6ef1761eb791f9ee9
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-08-18 10:59:03 +0300

    Time: 1131 ms (28.20%), Space: 36.5 MB (27.32%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,19 +1,14 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
         count = Counter(arr)
-        lst = []
-        
-        value_count = 0
-        for k, v  in count.items():
-            lst.append([-v, k])
-            
-        heapify(lst)
-        
-        ans = 0
-        for i in lst:
-            value_count += -heappop(lst)[0]
+                
+        value_count, ans = 0, 0
+        items = sorted(count.items(), key=lambda x:-x[1])       
+        for key, val in items:
+            value_count += val
             ans += 1
-            if value_count >= len(arr)//2:
+            
+            if value_count >= len(arr) // 2:
                 return ans
             
         
