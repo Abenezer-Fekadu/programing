d631ec3e6a09ab3167ee218611a2233948f55ae1
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-09-21 17:55:45 +0300

    Time: 147 ms (33.98%), Space: 14.3 MB (98.69%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key = lambda x: x[1])
+        
+        lst = []
+        for i in range(len(trips)):
+            p, s, e = trips[i]
+            while lst and lst[0][0] <= s:
+                capacity += heappop(lst)[1]
+            
+            if p <= capacity:
+                heappush(lst, [e, p])
+                capacity -= p
+            else:
+                return False
+            
+        return True
