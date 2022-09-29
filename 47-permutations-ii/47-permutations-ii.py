db72920c85282642aaba9077d611a1ae8cec2a2d
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-09-29 17:41:09 +0300

    Time: 951 ms (13.38%), Space: 14.5 MB (24.94%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,6 +1,7 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         ans = set()
+        
         def solve(idx, lst):
             if len(lst) == len(nums):
                 ans.add(tuple(lst))
@@ -17,14 +18,6 @@ class Solution:
                     idx.pop()
                 
             
-        perm = []
-        ind = []
-        for i in range(len(nums)):
-            perm.append(nums[i]) 
-            ind.append(i)
-            solve(ind, perm)    
-            perm.pop()
-            ind.pop()
-            
+        solve([], [])
         return ans
             
