1729d647ca1d44bb7e1e06db05bba20916367ee8
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-09-29 10:32:23 +0300

    Time: 1037 ms (5.01%), Space: 15.4 MB (80.93%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,5 +1,30 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        arr.sort(key=lambda n: (abs(x-n), n))
+        l, r = 0, k - 1
+        summ = 0
         
-        return sorted(arr[:k])
+        for i in range(k):
+            summ += abs(x-arr[i])
+            
+        minn = [summ, l, r]
+    
+        r += 1
+        while r < len(arr):
+            summ -= abs(x-arr[l])
+            summ += abs(x-arr[r])
+            l += 1
+            
+            if summ < minn[0]:
+                minn = [summ, l, r]
+                
+            r += 1
+            
+        return arr[minn[1]: minn[2]+1]
+            
+        
+        
+        
+
+        
+        
+        
