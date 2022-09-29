46efc8ae1b3bb92f6233db99d7d6ee987952dcd6
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-09-29 17:42:09 +0300

    Time: 87 ms (15.93%), Space: 14.2 MB (23.30%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,4 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(permutations(nums))
-        
+        ans = set()
+        def solve(arr):
+            if len(nums) == len(arr):
+                ans.add(tuple(arr))
+                return
+            
+            for num in nums:
+                if num not in arr:
+                    arr.append(num)
+                    solve(arr)
+                    arr.pop()
+
+        solve([])
+        return ans
