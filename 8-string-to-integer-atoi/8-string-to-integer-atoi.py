6939bd93db8b4647c79e3915dc8fe3f09a4718a5
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-10-09 15:05:49 +0300

    Time: 79 ms (13.03%), Space: 13.9 MB (78.21%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -18,12 +18,8 @@ class Solution:
             return 0
         num = 0 - float("".join(ans)) if op == '-' else float("".join(ans))
 
-        if num >= 2**31 - 1:
-            return 2**31 - 1
-        elif num <= -2**31:
-            return -2**31
         
-        return int(num)
+        return max(-2**31, min(int(num), 2**31-1))
             
         
         
