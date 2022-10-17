4b69cae198258d8224e2f2d1f48a5ab15ad89ae1
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-10-17 10:34:46 +0300

    Time: 55 ms (44.02%), Space: 13.9 MB (66.02%) - LeetHub

--- 1324-print-words-vertically/1324-print-words-vertically.py
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -1,29 +1,14 @@
 class Solution:
     def printVertically(self, s: str) -> List[str]:
-        size = 0
         words = s.split()
-        for word in words:
-            size = max(size, len(word)) 
-        ans = ["" for i in range(size)]
+        size = max(len(word) for word in words)
         
+        ans = ["" for i in range(size)]
         for word in words:
             for i in range(size):
                 if i < len(word):
                     ans[i] += word[i]
                 else:
                     ans[i] += " "
-        
-        lst = []
-        for word in ans:
-            t = 0
-            for i in range(len(word)-1, -1, -1):
-                if word[i] == " ":
-                    continue
-                else:
-                    t = i
-                    break
-            check = word[:t+1]
-            if check:
-                lst.append(check)
-                
-        return lst
+            
+        return [word.rstrip() for word in ans]
