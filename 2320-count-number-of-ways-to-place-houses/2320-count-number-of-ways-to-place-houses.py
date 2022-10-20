8db0eb152617a24fb2fcb60807ab873bd315a17d
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-10-20 05:36:57 -0700

    Time: 248 ms (60.05%), Space: 13.8 MB (82.18%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
@@ -1,12 +1,6 @@
 class Solution:
     def countHousePlacements(self, n: int) -> int:
-        check = {1: 2, 2: 3} 
-        def dp(i):
-            if i in check:
-                return check[i]
-            
-            check[i] = dp(i-1) + dp(i-2)
-            return check[i]
-    
-        return (dp(n)**2) % (10**9+7)
-    
+        x, y = 1, 2
+        for i in range(1, n):
+            x, y = y, x + y
+        return (y ** 2) % (10**9 + 7)
