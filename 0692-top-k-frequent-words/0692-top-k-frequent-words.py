ba6f3d38263b48bbf1995d71ae9eb822463ee6a8
Abenezer Fekadu <se.abenezer.fekadu@gmail.com>
2022-10-21 02:59:26 -0700

    Time: 107 ms (62.51%), Space: 14 MB (62.87%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,6 +1,11 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        count = Counter(words)
-        ans = sorted(count, key=lambda c: (-count[c], c))
+        dict1, bucket, ans = Counter(words), [[] for _ in range(len(words)+1)], []
+    
+        for i in range(len(words)+1):
+            bucket[i] = sorted([key for key, val in dict1.items() if dict1[key] == i])
+
+        for i in range(len(words)-1,-1,-1):
+            ans += bucket[i]
+
         return ans[:k]
- 
