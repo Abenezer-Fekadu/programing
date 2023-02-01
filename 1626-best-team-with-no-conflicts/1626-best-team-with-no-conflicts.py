c1acd5231d264b8bf5abb163465caf2b6e403aa5
Abeni27 <se.abenezer.fekadu@gmail.com>
2023-02-01 21:12:26 +0300

    Time: 2165 ms (52.45%), Space: 15.3 MB (6.29%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,8 +1,18 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        dp = [0]*(1+max(ages))                  
-                                            
-        score_age = sorted(zip(scores, ages))                                              
-        for score, age in score_age:       
-            dp[age] = score + max(dp[:age+1])                                        
-        return max(dp) 
+        lst = sorted(list(zip(ages, scores)))
+        memo = {}
+        memo[0] = lst[0][1]
+        def helper(i):
+            if i not in memo:
+                res = 0
+                for j in range(i):
+                    if lst[i][1] >= lst[j][1]:
+                        res = max(res, helper(j))
+                memo[i] =  res + lst[i][1]
+            return memo[i]
+        
+        ans = 0
+        for i in range(len(ages)):
+            ans = max(helper(i), ans)
+        return ans
