02c854d6ba6367c3762c71fd61ce50b77b60d6ff
Abeni27 <se.abenezer.fekadu@gmail.com>
2023-05-20 21:02:23 +0300

    Time: 44 ms (31.85%), Space: 16.5 MB (8.30%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+       	root = {}
+        def find(x):
+            p, xr = root.setdefault(x, (x, 1.0))
+            if x != p:
+                r, pr = find(p)
+                root[x] = (r, xr*pr)
+            return root[x]
+
+        def union(x, y, ratio):
+            px, xr, py, yr = *find(x), *find(y)
+            if not ratio:
+                return xr / yr if px == py else -1.0
+            if px != py:
+                root[px] = (py, yr/xr*ratio)
+
+                
+        for (x, y), v in zip(equations, values):
+            union(x, y, v)
+        return [union(x, y, 0) if x in root and y in root else -1.0 for x, y in queries]
